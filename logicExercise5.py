de01348d14a4a4111e98c226ddfc35782dee20b2
mfpachecog <manuelfpg012@gmail.com>
2025-11-04 23:11:01 -0500

    submit code to logicExercise5.py

---
+++ logicExercise5.py
@@ -0,0 +1,15 @@
+# python script that stores the value of three different temperatures
+# in celcius, converts them to fahrenheit, and prints 
+# the results
+
+temperature_1 = 20.1
+temperature_2 = 45.4
+temperature_3 = 37.3
+
+farenheit_1 = (temperature_1 * 1.8)+32
+farenheit_2 = (temperature_2 * 1.8)+32
+farenheit_3 = (temperature_3 * 1.8)+32
+
+print(f"The first Celcius temperature {temperature_1} in Farenheit is: {farenheit_1}")
+print(f"The second Celcius temperature {temperature_2} in Farenheit is: {farenheit_2}")
+print(f"The third Celcius temperature {temperature_3} in Farenheit is: {farenheit_3}")
